f877c86273851b8edfbaca45c5ce1c3ee8b1ae7f
shay_daniel <shay@isom.org>
2020-12-19 18:22:13 -0800

    filter and search complete

--- src/components/Directory.js
+++ src/components/Directory.js
@@ -14,26 +14,69 @@ class Directory extends Component {
       });
       console.log(peeps)
     });
+  };
+
+  sortFirst = () => {
+    var sortedFirst = [...this.state.users];
+    sortedFirst = sortedFirst.sort((a, b) => {
+      if (a.name.first < b.name.first) {
+        return -1;
+      }
+      if (a.name.first > b.name.first) {
+        return 1;
+      }
+      return 0
+    });
+    this.setState({
+      users: sortedFirst
+    })
+  }
+
+  sortLast = () => {
+    var sortedLast = [...this.state.users];
+    sortedLast = sortedLast.sort((a, b) => {
+      if (a.name.last < b.name.last) {
+        return -1;
+      }
+      if (a.name.last > b.name.last) {
+        return 1;
+      }
+      return 0
+    });
+    this.setState({
+      users: sortedLast
+    })
+  }
+
+  searchEmployees = (e) => {
+    console.log(e);
+    var searchedEmployees = this.state.users.filter(user => {
+      return user.name.first.toLowerCase().includes(e) ||  user.name.last.toLowerCase().includes(e)
+    });
+    this.setState({
+      users: searchedEmployees
+    })
   }
 
   render() {
-    return (
-    <div className="table-responsive">
-      <table className="table table-bordered table-dark table-hover table-sm table-striped">
-        <thead>
-          <tr>
-            <th>Photo</th>
-            <th>First</th>
-            <th>Last</th>
-            <th>Email</th>
-            <th>Address</th>
-            {/* <th>Age</th> */}
-            <th>Phone</th>
 
-          </tr>
-        </thead>
-        <tbody>
-          {this.state.users.map(employees => {
+    return (
+      <div className="table-responsive">
+        <input className="searchbar" placeholder="search for an employee" onChange = {(e) => this.searchEmployees(e.target.value)}></input>
+        <table className="table table-bordered table-dark table-hover table-sm table-striped">
+          <thead>
+            <tr>
+              <th>Photo</th>
+              <th>First<br></br><button size="sm" onClick={this.sortFirst} className="az sm">sort a-z</button></th>
+              <th>Last<br></br><button onClick={this.sortLast} className="az sm">sort a-z</button></th>
+              <th>Email</th>
+              <th>Address</th>
+              <th>Phone</th>
+
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.users.map(employees => {
               return (
                 <tr>
                   <td>
@@ -42,17 +85,18 @@ class Directory extends Component {
                   <td>{employees.name.last}</td>
                   <td>{employees.email}</td>
                   <td>{employees.location.street.number + " " + employees.location.street.name + ", " + employees.location.city + ", " + employees.location.state + " " + employees.location.postcode}</td>
-                  {/* <td>{employees.dob.age}</td> */}
                   <td>{employees.phone}</td>
-                  
+
                 </tr>
               )
-          })}
-        </tbody>
-      </table>
-    </div>
+            })}
+          </tbody>
+        </table>
+      </div>
     )
   }
 }
+
+
 
 export default Directory;
